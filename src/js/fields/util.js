624d3793c0b6d9572535c2a4d832fb4762bd90ae
Jan Martinek <honza.martinek@gmail.com>
2018-01-21 13:15:27 +0100

    Field descriptors refactor

--- src/js/fields/util.js
+++ src/js/fields/util.js
@@ -1,34 +1,51 @@
 const t = require('./../translator').translate('fields.util.');
 
 function activate(el, fieldName) {
-  const wrapper = cel('DIV', ['key-wrapper', 'input-group']);
-
-  // wrapper.appendChild(type);
-  if (el.classList.contains('name')) {
-    const label = cel('SPAN', ['input-group-label'], '<i class="fa fa-tag" aria-hidden="true"></i>');
-    wrapper.appendChild(label);
-    const name = cel('INPUT', ['name-setter', 'input-group-field']);
-    name.addEventListener('input', () => nameInput(el, name.value));
-    name.setAttribute('placeholder', t('name'));
-    wrapper.appendChild(name);
-  }
-  if (el.classList.contains('out')) {
-    el.setAttribute('readonly', 'readonly');
-  }
-  if (el.classList.contains('formula')) {
-    const label = cel('SPAN', ['input-group-label'], '&fnof;');
-    wrapper.appendChild(label);
-    const formula = cel('INPUT', ['formula-setter', 'input-group-field']);
-    formula.addEventListener('input', () => appendFormula(el, formula.value));
-    formula.setAttribute('placeholder', t('formula'));
-    wrapper.appendChild(formula);
-    el.setAttribute('readonly', 'readonly');
-  }
   el.dataset.fieldName = fieldName;
-  el.insertAdjacentElement('afterend', wrapper);
+
+  const showsOutput = el.classList.contains('out') || el.classList.contains('formula');
+  if (showsOutput) el.setAttribute('readonly', 'readonly');
+
+  const descriptors = createDescriptors(el);
+  if (descriptors) el.insertAdjacentElement('afterend', descriptors);
 }
 
-function cel(name, classNames, html) {
+
+function createDescriptors(el) {
+  let name;
+  let formula;
+
+  const descriptors = createElement('DIV', ['descriptors']);
+
+  if (el.classList.contains('name')) {
+    name = createDescriptor('name', '<i class="fa fa-tag" aria-hidden="true"></i>', nameInput, el);
+    descriptors.appendChild(name);
+  }
+
+  if (el.classList.contains('formula')) {
+    formula = createDescriptor('formula', '&fnof;=', appendFormula, el);
+    descriptors.appendChild(formula);
+  }
+
+  return (formula || name) ? descriptors : null;
+}
+
+function createDescriptor(type, labelHtml, listener, input) {
+  const descriptor = createElement('SPAN', ['descriptor', `${type}-descriptor`]);
+
+  const label = createElement('SPAN', ['descriptor-label'], labelHtml);
+  descriptor.appendChild(label);
+
+  const field = createElement('INPUT', ['descriptor-value', `${type}-setter`]);
+  field.addEventListener('input', () => listener(input, field.value));
+  field.setAttribute('placeholder', t(type));
+  descriptor.appendChild(field);
+
+  return descriptor;
+}
+
+
+function createElement(name, classNames, html) {
   const el = document.createElement(name);
   if (classNames) el.setAttribute('class', classNames.join(' '));
   if (html) el.innerHTML = html;
